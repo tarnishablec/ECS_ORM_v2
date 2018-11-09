b04cd6c8d2e3d128f5b74a226013acdf64bbe45c
tarnishablec <tarnishablec@outlook.com>
2018-11-09 14:43:50 +0800

    Select Done

--- src/Test/Test1.java
+++ src/Test/Test1.java
@@ -5,13 +5,5 @@ import ECScore.EntityManager;
 public class Test1 {
     public static void main(String[] args) {
         EntityManager.init();
-
-//
-//        Filter filter = new Filter(Account.class,Appoint.class);
-//        ArrayList<Entity> entities= EntityManager.loadEntities(filter);
-//        System.out.println(entities.size());
-//        for (Entity e:entities){
-//            System.out.println(EntityManager.getComponentData(e,Account.class).id);
-//        }
     }
 }
